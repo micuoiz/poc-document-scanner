7a1db09ec55c8c32bebb018e2dcab170d9c690c2
Miroslav Cutic <miroslav.cutic@cleoag.ch>
2023-07-12 19:16:21 +0200

    Added first implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc-screenshot';
+
+  importFile(event) {
+    if (event.target.files.length === 0) {
+      console.log('No file selected!');
+      return;
+    }
+    let file: File = event.target.files[0];
+    console.log(file);
+    // after here 'file' can be accessed and used for further process
+  }
 }
