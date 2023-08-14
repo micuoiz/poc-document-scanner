d47db048491bcf8f53ff955e667885c3895e756f
micuoiz <142210564+micuoiz@users.noreply.github.com>
2023-08-14 15:36:12 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,69 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { applyPolyfills, defineCustomElements } from "@oiz/stzh-components/loader";
+
+// load the polyfills if you need to support older browsers
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
+
+declare var jscanify: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'poc-document-scanner';
+  files: File[] = [];
+
+  @ViewChild('canvas')
+  private canvas: ElementRef = {} as ElementRef;
+
+  @ViewChild('result')
+  private result: ElementRef = {} as ElementRef;
+
+  @ViewChild('video')
+  private video: ElementRef = {} as ElementRef;
+
+  importFile(event: any) {
+    if (event.target.files.length === 0) {
+      console.log('No file selected!');
+      return;
+    }
+    let file: File = event.target.files[0];
+    this.files.push(file);
+  }
+
+  downloadFile(file: File) {
+    const blob = new Blob([file], { type: file.type });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = file.name;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  openCamera(): void {
+    const scanner = new jscanify();
+    const canvasCtx = this.canvas.nativeElement.getContext('2d');
+    const resultCtx = this.result.nativeElement.getContext("2d");
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      this.video.nativeElement.srcObject = stream;
+      this.video.nativeElement.onloadedmetadata = () => {
+        this.video.nativeElement.play();
+        setInterval(() => {
+          canvasCtx.drawImage(this.video.nativeElement, 0, 0);
+          const resultCanvas = scanner.highlightPaper(this.canvas.nativeElement);
+          resultCtx.drawImage(resultCanvas, 0, 0);
+        }, 10);
+      };
+    });
+  }
+}
